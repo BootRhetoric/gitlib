1ef9bd6212032cf47d969f15c9ee072348c128b6
BootRhetoric <110117466+BootRhetoric@users.noreply.github.com>
2023-11-20 14:59:27 +0100

    added build deps to shell.nix

--- hlibgit2/shell.nix
+++ hlibgit2/shell.nix
@@ -3,7 +3,19 @@ let
   c2hsc = pkgs.haskellPackages.callCabal2nix "c2hsc" (builtins.fetchGit {url = "https://github.com/jwiegley/c2hsc.git";}) {};
 in
   pkgs.mkShell {
-    packages = [
-      c2hsc
-    ];
+    packages =
+      [
+        c2hsc
+      ]
+      ++ (with pkgs; [
+        cmake
+        pkg-config
+        python3
+        zlib
+        libssh2
+        openssl
+        pcre
+        http-parser
+        libiconv
+      ]);
   }
